8fae3194f39c6261636cc2bd17de163e2db85367
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-02-24 16:49:34 +1000

    feat: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(minval, maxval, res) {
+      this.minval;
+      this.maxval;
+      this.res;
+    }
+  
     setRange(min, max) {
-
+        this.minval = min;
+        this.maxval = max;
     }
-
+  
     guess() {
-
+         this.res =((this.maxval-this.minval) / 2);
+         return (this.minval + Math.ceil(this.res));
+      
     }
-
+  
     lower() {
-
+        this.maxval = this.maxval - Math.floor(this.res);
     }
-
+  
     greater() {
-
+        this.minval = this.minval + Math.ceil(this.res);
     }
-}
+  }
 
 module.exports = GuessingGame;
